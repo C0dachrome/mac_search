a0ea68dc705f509c21fb56a6537e097ccfd04490
C0dachrome <codycmillward@gmail.com>
2026-02-23 21:59:37 -0700

    syntax error fix

--- startKiosk.sh
+++ startKiosk.sh
@@ -5,12 +5,12 @@ sudo rfkill unblock all
 sleep 1
 sudo ifconfig "$interface" up
 
-# Start the Python API in the background
+rm -f /home/codyc/mac_search/scan-*.csv
+
 python3 /home/codyc/mac_search/macSearchAPI.py > /home/codyc/api.log 2>&1 &
 
-sudo airodump-ng -w scan --write-interval 1 --output-format csv "$interface" &
+sudo airodump-ng -w scan --write-interval 1 --output-format csv "$interface" > /dev/null 2>&1 &
 
 sleep 3
 
-# Launch the browser in fullscreen directed at the API
-xinit /usr/bin/chromium --kiosk --incognito--app=http://localhost:5000 --user-data-dir=/home/codyc/.chrome-kiosk
+xinit /usr/bin/chromium --kiosk --incognito --no-sandbox --app=http://localhost:5000 --user-data-dir=/home/codyc/.chrome-kiosk --nocursor
