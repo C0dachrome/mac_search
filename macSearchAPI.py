1b8caf0e0b2f700ce794f546f459575d725f89f9
C0dachrome <codycmillward@gmail.com>
2026-02-24 20:31:44 -0700

    idk

--- macSearchAPI.py
+++ macSearchAPI.py
@@ -1,10 +1,14 @@
 import asyncio
 from quart import Quart, render_template, jsonify
+from quart_cors import cors
 from pyrcrack import AirodumpNg
 
 app = Quart(__name__)
-air = AirodumpNg()
+app = cors(app, allow_origin="*")
+
 INTERFACE = 'wlan1'
+air = AirodumpNg()
+TARGET_MAC = None
 
 @app.route('/')
 async def index():
@@ -12,24 +16,48 @@ async def index():
 
 @app.route('/get_data')
 async def get_data():
-    devices = []
-    for ap in air.get_count():
-        devices.append({
-            "MAC": ap.bssid,
-            "Power": ap.dbm,
-            "Channel": ap.channel,
-            "Name": ap.essid or "Unknown AP"
-        })
-    
-    sorted_devices = sorted(devices, key=lambda x: x['Power'], reverse=True)[:5]
-    return jsonify(sorted_devices)
+    global TARGET_MAC
+    try:
+        results = air.get_count()
+        devices = []
+        
+        if TARGET_MAC:
+            for ap in results:
+                if ap.bssid.upper() == TARGET_MAC:
+                    return jsonify([{
+                        "MAC": ap.bssid,
+                        "Power": ap.dbm,
+                        "Channel": ap.channel,
+                        "Name": ap.essid or "Unknown",
+                        "IsTarget": True
+                    }])
 
-async def start_airodump():
+        for ap in results:
+            devices.append({
+                "MAC": ap.bssid,
+                "Power": ap.dbm,
+                "Channel": ap.channel,
+                "Name": ap.essid or "Unknown"
+            })
+        
+        return jsonify(sorted(devices, key=lambda x: x['Power'], reverse=True)[:5])
+    except:
+        return jsonify([])
+
+@app.route('/start_target/<ch>/<mac>')
+async def start_target(ch, mac):
+    global TARGET_MAC
+    TARGET_MAC = mac.upper()
+    return jsonify({"status": "tracking", "mac": mac})
+
+async def run_scanner():
     async with air(INTERFACE) as scanner:
         while True:
             await asyncio.sleep(1)
 
+@app.before_serving
+async def startup():
+    app.add_background_task(run_scanner)
+
 if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    loop.create_task(start_airodump())
     app.run(host='0.0.0.0', port=5000)
